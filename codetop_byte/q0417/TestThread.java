c6b6fd372531e1e0c77140b4018a4a9eae9cffe9
qrx <1330342818@qq.com>
2023-04-22 21:17:07 +0800

    20230422

--- codetop_byte/q0417/TestThread.java
+++ codetop_byte/q0417/TestThread.java
@@ -1,6 +1,8 @@
 package codetop_byte.q0417;
 
+import java.util.Timer;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -18,15 +20,29 @@ public class TestThread {
         threadPoolExecutor.execute(new Runnable() {
             @Override
             public void run() {
-
+                System.out.println("hh");
             }
         });
-
+        Thread t = new Thread(){
+            @Override
+            public void run() {
+                super.run();
+            }
+        };
         Lock reentrantLock = new ReentrantLock();
         ReadWriteLock lock = new ReentrantReadWriteLock();
         CountDownLatch count = new CountDownLatch(2);
-
-
+        Semaphore semaphore = new Semaphore(10);
+        AtomicInteger atomicInteger = new AtomicInteger(10);
+        int a = atomicInteger.addAndGet(1);
+        Timer timer = new Timer();
+        timer.schedule();
+        System.out.println(a);
+//        try {
+//            semaphore.acquire();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
 
     }
 }
