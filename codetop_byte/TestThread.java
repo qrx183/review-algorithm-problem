570dd187ea65b6d55caa7b7e930ccdbcf93fdae5
qrx <1330342818@qq.com>
2023-05-22 21:36:04 +0800

    20230522'

--- codetop_byte/TestThread.java
+++ codetop_byte/TestThread.java
@@ -1,5 +1,6 @@
 package codetop_byte;
 
+import java.text.SimpleDateFormat;
 import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -9,44 +10,96 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
+class Service1 {
 
+    public void process(String name) {
+        User user = new User("超哥");
+        UserContextHolder.holder.set(user);
+        new Service2().process();
+    }
+}
+
+class Service2 {
+
+    public void process()  {
+        User user = UserContextHolder.holder.get();
+        ThreadSafeFormatter.dateFormatThreadLocal.get();
+        System.out.println("Service2拿到用户名：" + user.name);
+        new Service3().process();
+        Runnable a = new Runnable() {
+            @Override
+            public void run(){
+                System.out.println("aaa");
+            }
+        };
+        Callable<Integer> c = new Callable<Integer>() {
+            @Override
+            public Integer call() throws Exception {
+                return null;
+            }
+        };
+
+    }
+}
+
+class ThreadSafeFormatter {
+
+    public static ThreadLocal<SimpleDateFormat> dateFormatThreadLocal = new ThreadLocal<SimpleDateFormat>() {
+        @Override
+        protected SimpleDateFormat initialValue() {
+            return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        }
+    };
+
+    public static ThreadLocal<SimpleDateFormat> dateFormatThreadLocal2 = ThreadLocal
+            .withInitial(() -> new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+}
+
+class Service3 {
+
+    public void process() {
+        User user = UserContextHolder.holder.get();
+        System.out.println("Service3拿到用户名：" + user.name);
+        UserContextHolder.holder.remove();
+    }
+}
+
+class UserContextHolder {
+
+    public static ThreadLocal<User> holder = new ThreadLocal<>();
+
+
+}
+
+class User {
+
+    String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+}
 public class TestThread {
     static int cnt = 0;
     static final Object object = new Object();
     static final CountDownLatch count = new CountDownLatch(10);
     // 这个方法相当于是建立一个具有初始值的ThreadLocal. 实际上是通过多态的向上转型,调用的initValue是SuppliedThreadLocal中的方法
-    static final ThreadLocal<String> threadLocal = ThreadLocal.withInitial(()->"qiuruixiang");
+    static final ThreadLocal<String> threadLocal = ThreadLocal.withInitial(() -> "qiuruixiang");
 
-    class A extends B{
+    class A extends B {
         int b = a;
 
     }
-    class B{
-       int a;
+
+    class B {
+        int a;
     }
 
     public static void main(String[] args) {
-        ReentrantLock reentrantLock = new ReentrantLock(true);
-        reentrantLock.tryLock();
-        Thread t = new Thread(()->{
-           while(true) {
-               try {
-                   Thread.sleep(1000);
-               } catch (InterruptedException e) {
-                   System.out.println("aa");
-                   Thread.currentThread().interrupt();
-               }
+        new Service1().process("");
 
-           }
-        });
-        t.start();
-        t.interrupt();
-        try {
-            t.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
     }
+
 
     public static void main2(String[] args) throws Exception {
         class MyThread extends Thread {
